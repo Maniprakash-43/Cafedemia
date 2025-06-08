d1a65828266429337da8f6b213996d719ccba489
Maniprakash-43 <maniprakashgugulothu25@gmail.com>
2025-06-08 12:39:33 +0530

    Update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -7,7 +7,7 @@ const addContainer = document.querySelector(".add-container");
 const removeContainer = document.querySelector(".remove-container");
 
 // Admin Side URL
-const adminSRC = "admin-side.html";
+const adminSRC = "http://127.0.0.1:5502/admin-side.html";
 
 // --- Admin Side ---
 if (adminSRC === window.location.href) {
